5dacdfbd611edfd26dd6f7aa1a705c8470f4ea46
Sam Benning <bensam1993@gmail.com>
2021-12-05 10:33:20 -0500

    Added lables and inputs for add book form.

--- displayAddBookForm.js
+++ displayAddBookForm.js
@@ -8,20 +8,61 @@ const displayAddBookForm = () => {
   container.appendChild(formHeader)
   container.appendChild(form)
 
-
+  //Add Title label and field
   const bookTitleLabel = document.createElement('label');
   const bookTitleField = document.createElement('input')
-  bookTitleLabel.innerText = 'Title';
+  bookTitleLabel.innerText = 'Title:';
   bookTitleField.setAttribute('id', 'bookTitle');
   bookTitleLabel.setAttribute('for', 'bookTitle');
 
   form.appendChild(bookTitleLabel);
   form.appendChild(bookTitleField);
+  form.appendChild(document.createElement('br'));
 
+  //Add Author label and field
   const bookAuthorLabel = document.createElement('label');
-  const bookPagesLabel = document.createElement('label');
-  const bookHasReadLabel = document.createElement('label');
+  const bookAuthorField = document.createElement('input');
+  bookAuthorLabel.innerText = 'Author:'
+  bookAuthorField.setAttribute('id', 'bookAuthor');
+  bookAuthorLabel.setAttribute('for', 'bookAuthor');
 
+  form.appendChild(bookAuthorLabel);
+  form.appendChild(bookAuthorField);
+  form.appendChild(document.createElement('br'));
+
+  //Add Pages label and field
+  const bookPagesLabel = document.createElement('label');
+  const bookPagesField = document.createElement('input');
+  bookPagesLabel.innerText = 'Pages:'
+  bookPagesField.setAttribute('id', 'bookPages');
+  bookPagesLabel.setAttribute('for', 'bookPages');
+
+  form.appendChild(bookPagesLabel);
+  form.appendChild(bookPagesField);
+  form.appendChild(document.createElement('br'));
+
+  //Add hasRead label and field
+  const bookHasReadLabel = document.createElement('label');
+  const bookHasReadCheck = document.createElement('input');
+  bookHasReadCheck.type = 'checkbox';
+  bookHasReadLabel.innerText = 'Read';
+  bookHasReadCheck.setAttribute('id', 'bookRead');
+  bookHasReadLabel.setAttribute('for', 'bookRead');
+
+  form.appendChild(bookHasReadLabel);
+  form.appendChild(bookHasReadCheck);
+  form.appendChild(document.createElement('br'));
+
+  //Add submit button
+  const submitBtn = document.createElement('input');
+  submitBtn.type = 'submit';
+  submitBtn.value = 'Add';
+
+  form.appendChild(submitBtn);
+
+
+  //Append the container div for the form to body
+  //Apply blur class to card-area
   body.appendChild(container);
   cardArea.classList.add('blur')
 }
